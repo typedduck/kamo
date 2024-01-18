1f92737a04a2baedeabafe4f8c1a4bc90ea80ac2
typedduck <typedduck@posteo.net>
2024-01-18 13:18:17 +0000

    Update version to 0.5.0, include tests directory, and add macros feature

--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 [![Crates.io](https://img.shields.io/crates/v/kamo)](https://crates.io/crates/kamo)
 [![Crates.io](https://img.shields.io/crates/d/kamo)](https://crates.io/crates/kamo)
 
-This is the release of the second part of the Kamo project: memory management
+Kamo (カモ) is japanese for duck.
+
+* This is a supplement to the second part of the Kamo project: memory management
+and runtime values. It adds procedural macros to parse s-expressions into
+runtime values. The macros are defined in the crate
+[`kamo-macros`](https://crates.io/crates/kamo-macros).
+
+* This is the release of the second part of the Kamo project: memory management
 and runtime values.
 
-The first part was the parser combinator library.
-
-Kamo (カモ) is japanese for duck.
+* The first part was the parser combinator library.
 
 ## Memory Management Module `kamo::mem`
 
@@ -218,6 +223,94 @@ fn parse_hexadecimal_natural(input: Input<'_>) -> ParseResult<u64> {
 fn parse_natural(input: Input<'_>) -> ParseResult<u64> {
     literal::natural(literal::Radix::Decimal)(input)
 }
+```
+
+## Macros
+
+The macros are used to parse a string literal into a single or an array of
+`kamo::value::Value`s. The macros are defined as follows:
+
+- `sexpr!(<mutator>, <expression>)` - A macro for parsing a single
+  s-expression from a string. It returns a `kamo::value::Value`.
+- `sexpr_file!(<mutator>, <filename>)` - A macro for parsing multiple
+  s-expressions from a file. It returns an array of `kamo::value::Value`s.
+  The array may be empty.
+ 
+- `sexpr_script!(<mutator>, <expressions>)` - A macro for parsing multiple
+  s-expressions from a string. It returns an array of `kamo::value::Value`s.
+  The array may be empty.
+ 
+The macros all take an optional `MutatorRef` identifier. This is used to allocate
+values on the heap. If the expression does not contain any values that need to
+be allocated on the heap, then the `Mutator` identifier can be omitted.
+ 
+The syntax for the macros is as defined by the Scheme standard R7RS for the
+`read` procedure. The syntactic definition is the `<datum>` in section
+["7.1.2 External representations"](https://standards.scheme.org/official/r7rs.pdf)
+of the standard.
+ 
+The syntax deviations from the standard are:
+ 
+- The extactness (`#e` and `#i`) of numbers is not supported. Floating-point
+  numbers are always inexact and integers are always exact.
+ 
+- Numbers may only be signed 64-bit integers or IEEE 754 double precision
+  floating-point numbers. The standard allows for arbitrary precision
+  integers, rationals and complex numbers.
+ 
+- Labels are not supported.
+ 
+- The `#;` comment syntax is only supported in the macros which parse
+  multiple s-expressions. The `#;` comment syntax may not be nested.
+ 
+- Character literals defined by a hex escape sequence may have 1 to 6
+  digits. The standard excepts 1 or more digits. The code must be a valid
+  Unicode code point.
+ 
+The parser is implemented with the [`pest`](https://crates.io/crates/pest)
+crate. The grammar is defined in `src/sexpr/sexpr.pest`. This is necessary
+because the combination parser library defined in `kamo::parser` cannot be
+used here. It would be cyclic dependency. There will be an implementation
+of a parser for s-expressions in the `kamo::form` module in the future. It
+will be based on the `kamo::parser` crate and will be used by the scheme
+interpreter.
+ 
+## Examples
+ 
+```rust
+use kamo::{mem::Mutator, sexpr, value::{print, Value}};
+ 
+let m = Mutator::new_ref();
+let value = sexpr!(m, "(1 2 3)");
+ 
+assert_eq!(print(value).to_string(), "(1 2 3)");
+```
+ 
+```rust
+use kamo::{mem::Mutator, sexpr_file, value::{print, Value}};
+ 
+let m = Mutator::new_ref();
+let values = sexpr_file!(m, "tests/sexpr/values.scm");
+ 
+assert_eq!(values.len(), 3);
+assert_eq!(print(values[0].clone()).to_string(), "()");
+assert_eq!(print(values[1].clone()).to_string(), "(100)");
+assert_eq!(print(values[2].clone()).to_string(), "#t");
+
+let values = sexpr_file!("tests/sexpr/empty.scm");
+assert_eq!(values.len(), 0);
+```
+ 
+```rust
+use kamo::{mem::Mutator, sexpr_script, value::{print, Value}};
+ 
+let m = Mutator::new_ref();
+let values = sexpr_script!(m, "(define a 1)\n(define b 2)\n(+ a b)");
+ 
+assert_eq!(values.len(), 3);
+assert_eq!(print(values[0].clone()).to_string(), "(define a 1)");
+assert_eq!(print(values[1].clone()).to_string(), "(define b 2)");
+assert_eq!(print(values[2].clone()).to_string(), "(+ a b)");
 ```
 
 ## Feature List
--- src/lib.rs
+++ src/lib.rs
@@ -230,6 +230,98 @@
 //! }
 //! ```
 //! 
+//! # Macros
+//! 
+//! Macros are available when the `macros` feature is enabled. It is not
+//! enabled by default.
+//! 
+//! The macros are used to parse a string literal into a single or an array of
+//! `kamo::value::Value`s. The macros are defined as follows:
+//! 
+//! - `sexpr!(<mutator>, <expression>)` - A macro for parsing a single
+//!   s-expression from a string. It returns a `kamo::value::Value`.
+//!
+//! - `sexpr_file!(<mutator>, <filename>)` - A macro for parsing multiple
+//!   s-expressions from a file. It returns an array of `kamo::value::Value`s.
+//!   The array may be empty.
+//!  
+//! - `sexpr_script!(<mutator>, <expressions>)` - A macro for parsing multiple
+//!   s-expressions from a string. It returns an array of `kamo::value::Value`s.
+//!   The array may be empty.
+//!  
+//! The macros all take an optional `MutatorRef` identifier. This is used to allocate
+//! values on the heap. If the expression does not contain any values that need to
+//! be allocated on the heap, then the `Mutator` identifier can be omitted.
+//!  
+//! The syntax for the macros is as defined by the Scheme standard R7RS for the
+//! `read` procedure. The syntactic definition is the `<datum>` in section
+//! ["7.1.2 External representations"](https://standards.scheme.org/official/r7rs.pdf)
+//! of the standard.
+//!  
+//! The syntax deviations from the standard are:
+//!  
+//! - The extactness (`#e` and `#i`) of numbers is not supported. Floating-point
+//!   numbers are always inexact and integers are always exact.
+//!  
+//! - Numbers may only be signed 64-bit integers or IEEE 754 double precision
+//!   floating-point numbers. The standard allows for arbitrary precision
+//!   integers, rationals and complex numbers.
+//!  
+//! - Labels are not supported.
+//!  
+//! - The `#;` comment syntax is only supported in the macros which parse
+//!   multiple s-expressions. The `#;` comment syntax may not be nested.
+//!  
+//! - Character literals defined by a hex escape sequence may have 1 to 6
+//!   digits. The standard excepts 1 or more digits. The code must be a valid
+//!   Unicode code point.
+//!  
+//! The parser is implemented with the [`pest`](https://crates.io/crates/pest)
+//! crate. The grammar is defined in `src/sexpr/sexpr.pest`. This is necessary
+//! because the combination parser library defined in `kamo::parser` cannot be
+//! used here. It would be cyclic dependency. There will be an implementation
+//! of a parser for s-expressions in the `kamo::form` module in the future. It
+//! will be based on the `kamo::parser` crate and will be used by the scheme
+//! interpreter.
+//!  
+//! ## Examples
+//!  
+//! ```rust
+//! use kamo::{mem::Mutator, sexpr, value::{print, Value}};
+//!  
+//! let m = Mutator::new_ref();
+//! let value = sexpr!(m, "(1 2 3)");
+//!  
+//! assert_eq!(print(value).to_string(), "(1 2 3)");
+//! ```
+//!  
+//! ```rust
+//! use kamo::{mem::Mutator, sexpr_file, value::{print, Value}};
+//!  
+//! let m = Mutator::new_ref();
+//! let values = sexpr_file!(m, "tests/sexpr/values.scm");
+//!  
+//! assert_eq!(values.len(), 3);
+//! assert_eq!(print(values[0].clone()).to_string(), "()");
+//! assert_eq!(print(values[1].clone()).to_string(), "(100)");
+//! assert_eq!(print(values[2].clone()).to_string(), "#t");
+//! 
+//! let values = sexpr_file!("tests/sexpr/empty.scm");
+//! assert_eq!(values.len(), 0);
+//! ```
+//!  
+//! ```rust
+//! use kamo::{mem::Mutator, sexpr_script, value::{print, Value}};
+//!  
+//! let m = Mutator::new_ref();
+//! let values = sexpr_script!(m, "(define a 1)\n(define b 2)\n(+ a b)");
+//!  
+//! assert_eq!(values.len(), 3);
+//! assert_eq!(print(values[0].clone()).to_string(), "(define a 1)");
+//! assert_eq!(print(values[1].clone()).to_string(), "(define b 2)");
+//! assert_eq!(print(values[2].clone()).to_string(), "(+ a b)");
+//! ```
+//! 
 //! # Feature List
 //! 
 //! - [x] Module `kamo::parser` for parsing UTF-8 text. A parser combinator
@@ -256,3 +348,77 @@
 pub mod mem;
 pub mod parser;
 pub mod value;
+
+#[cfg(feature = "macros")]
+pub use kamo_macros::{sexpr, sexpr_file, sexpr_script};
+
+#[cfg(all(test, feature = "macros"))]
+mod tests {
+    use super::*;
+    use super::{
+        mem::Mutator,
+        value::{print, Value},
+    };
+
+    #[test]
+    fn sexpr_macro() {
+        let m = Mutator::new_ref();
+
+        let value = sexpr!(r#"()"#);
+        assert_eq!(print(value).to_string(), r#"()"#);
+
+        let value = sexpr!(r#"#true"#);
+        assert_eq!(print(value).to_string(), r#"#t"#);
+
+        let value = sexpr!(r#"#t"#);
+        assert_eq!(print(value).to_string(), r#"#t"#);
+
+        let value = sexpr!(r#"#false"#);
+        assert_eq!(print(value).to_string(), r#"#f"#);
+
+        let value = sexpr!(r#"#f"#);
+        assert_eq!(print(value).to_string(), r#"#f"#);
+
+        let value = sexpr!(r#"#\a"#);
+        assert_eq!(print(value).to_string(), r#"#\a"#);
+
+        let value = sexpr!(r#"#\a"#);
+        assert_eq!(print(value).to_string(), r#"#\a"#);
+
+        let value = sexpr!(r#"100"#);
+        assert_eq!(print(value).to_string(), r#"100"#);
+
+        let value = sexpr!(r#".5"#);
+        assert_eq!(print(value).to_string(), r#"0.5"#);
+
+        let value = sexpr!(r#"+inf.0"#);
+        assert_eq!(print(value).to_string(), r#"+inf.0"#);
+
+        let value = sexpr!(r#"#b101"#);
+        assert_eq!(print(value).to_string(), r#"5"#);
+
+        let value = sexpr!(m, r#""Hello World!""#);
+        assert_eq!(print(value).to_string(), r#""Hello World!""#);
+
+        let value = sexpr!(m, r#"hello"#);
+        assert_eq!(print(value).to_string(), r#"hello"#);
+
+        let value = sexpr!(m, r#"#u8( 1 2 3)"#);
+        assert_eq!(print(value).to_string(), r#"#u8(1 2 3)"#);
+
+        let value = sexpr!(m, r#"'(+ 1 2)"#);
+        assert_eq!(print(value).to_string(), r#"(quote (+ 1 2))"#);
+
+        let value = sexpr!(m, r#",(+ 1 2)"#);
+        assert_eq!(print(value).to_string(), r#"(unquote (+ 1 2))"#);
+
+        let value = sexpr!(m, r#"`(+ 1 2)"#);
+        assert_eq!(print(value).to_string(), r#"(quasiquote (+ 1 2))"#);
+
+        let value = sexpr!(m, r#",@(+ 1 2)"#);
+        assert_eq!(print(value).to_string(), r#"(unquote-splicing (+ 1 2))"#);
+
+        let value = sexpr!(m, r#"#(+ 1 2)"#);
+        assert_eq!(print(value).to_string(), r#"#(+ 1 2)"#);
+    }
+}
