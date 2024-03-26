53c7cb07615ef5cf3d091b8f99d7da90ecbeaa45
typedduck <typedduck@posteo.net>
2024-03-26 14:49:27 +0000

    Edited doc-tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
 //! A library to assist in the creation of an interpreter or compiler and its
 //! associated runtime.
-//! 
+//!
 //! This is the second release of the Kamo project. This crate contains the
 //! parser library and the memory management library, which also includes the
 //! runtime representation of values.
-//! 
+//!
 //! ## Memory Management
-//! 
+//!
 //! The [`Mutator`](crate::mem::Mutator) implements automatic memory management.
 //! It holds an [`Arena`](crate::mem::Arena) for each type. Memory collection is
 //! done by a mark and sweep garbage collector.
-//! 
+//!
 //! The mutator implementation is thread local. The arena allocator is
 //! implemented as a vector of [`Bucket`](crate::mem::Bucket)s. Each bucket
 //! holds a vector of [`Slot`](crate::mem::Slot)s. The slots are used to store
 //! the values. Buckets have a fixed size and are allocated on the heap. The
 //! buckets are allocated on demand when the slots are full.
-//! 
+//!
 //! The hierarchy of types is as follows:
-//! 
+//!
 //! * Mutator contains Arenas.
 //! * Arena contains Buckets.
 //! * Bucket contains Slots.
 //! * Slot contains a value.
 //! * a value is represented by a Pointer.
-//! 
+//!
 //! Values managed by the mutator must be traceable. The mutator uses the
 //! [`Trace`](crate::mem::Trace) trait to trace the values. The `Trace` trait is
 //! implemented for all types that can contain values managed by the mutator.
-//! 
+//!
 //! [`Pointer`](crate::mem::Pointer)s use reference counting to keep track of
 //! the number of references to the value. When the number of references reaches
 //! zero the value may be subject to garbage collection. To avoid cycles
@@ -37,7 +37,7 @@
 //! Instead they are traced by the mutator when memory collection is done. This
 //! is the reason why a drop of the reference count of a pointer to zero does
 //! not immediately free the value.
-//! 
+//!
 //! The garbage collector is implemented as a mark and sweep collector,
 //! [`collect_garbage()`](crate::mem::Mutator::collect_garbage()). All values
 //! which hold a lock are roots. The garbage collector traces all roots and
@@ -46,84 +46,84 @@
 //! repeatedly calling the [`trace()`](crate::mem::Trace::trace()) method on the
 //! values and adding the values to a work list. This continues until the work
 //! list is empty.
-//! 
+//!
 //! ### `Allocator` Trait
-//! 
+//!
 //! The mutator uses the global allocator to allocate memory for buckets,
 //! arenas, vectors and strings etc. Dynamic memory allocation is done by the
 //! allocator provided by the [`std::alloc`] crate. Since the implementation of
 //! the allocator provided by the standard library is highly optimized it would
 //! be a waste of effort to reimplement it.
-//! 
+//!
 //! As a consequence the mutator is not able to free memory when the global
 //! allocator fails to allocate memory. This is because the mutator does not
 //! know which memory is allocated by the global allocator. The mutator only
 //! knows which memory is allocated by its arenas. Collection of memory is
-//! triggered only by the allocation pressure. 
-//! 
+//! triggered only by the allocation pressure.
+//!
 //! When the allocator trait is stabilized the mutator will be able to use the
 //! the trait to mitigate allocation failures by freeing memory when the global
 //! allocator fails.
-//! 
+//!
 //! ## Runtime Values
-//! 
+//!
 //! [`Value`](crate::value::Value)s can either hold immediate values or pointers
 //! to values allocated in the mutator.
-//! 
+//!
 //! The following immediate values are supported:
-//! 
+//!
 //! * `nil`, the empty list
 //! * `bool`
 //! * `char`, a Unicode scalar value
 //! * `i64`
 //! * `f64`
-//! 
+//!
 //! The following pointer values are supported:
-//! 
+//!
 //! * `pair`, a pair of values, also known as `Cons` cell
 //! * `symbol`, a symbol, an interned string
 //! * `string`, a UTF-8 string
 //! * `vector`, a vector of values
 //! * `bytevector`, a vector of bytes
-//! 
+//!
 //! Value provides a safe interface to the values in the runtime. It is
 //! implemented as a wrapper around [`ValueKind`](crate::value::ValueKind). It
 //! provides methods to convert the value into a Rust type, to check the type of
 //! the value, and to visit the value. Heap values are automatically locked and
 //! unlocked when necessary.
-//! 
+//!
 //! Value implements the visitor pattern. The [`Visitor`](crate::value::Visitor)
 //! trait is used to visit the value. When implementing the `Visitor` trait it
 //! must be considered that the value may be cyclic. The visitor must keep track
 //! of the values it has already visited to avoid infinite loops.
-//! 
+//!
 //! The `Visitor` trait is used to implement printing of values. The `Value`
 //! itself does not implement printing. Instead the
 //! [`Display`](std::fmt::Display) trait is implemented for a specific vistor.
 //! This makes it possible to implement different printing strategies for
 //! values.
-//! 
+//!
 //! An implementation of the `Visitor` trait is provided to print values in a
 //! Scheme-like syntax,
 //! [`SimplePrinterVisitor`](crate::value::SimplePrinterVisitor).
-//! 
+//!
 //! ## Combination Parser
-//! 
+//!
 //! The parser library is a general purpose parser library that can be used to
 //! parse any language. It is designed to be used in conjunction with the future
 //! parts of this project, but it can also be used on its own.
-//! 
+//!
 //! The memory management library is a library for automatic memory management.
 //! Values are allocated in a mutator which holds an arena allocator for each
 //! type. Memory collection is done by a mark and sweep garbage collector.
-//! 
+//!
 //! The future parts of this project will extend this crate with a library for
 //! compile time evaluation and transformation into an intermediate
 //! representation, an implemenation of an interpreter for the intermediate
 //! representation, and a reference implementation of Scheme. The implementation
 //! will be based on the R7RS standard and will implement a subset of the
 //! standard.
-//! 
+//!
 //! The design goal of this project is to create a framework that can be used to
 //! create a language interpreter or compiler. AST nodes will be created by the
 //! parser and then passed to the evaluator. The AST is represented as symbolic
@@ -132,24 +132,24 @@
 //! intermediate representation. This is the compile time representation of the
 //! program. The intermediate representation can then be either interpreted or
 //! compiled into a target representation.
-//! 
+//!
 //! # Parser Example
-//! 
+//!
 //! Example of a parser that parses a Scheme-like byte-vector and returns it as
 //! `Vec<u8>`.
-//! 
+//!
 //! The grammar is defined as follows:
-//! 
+//!
 //! ```text
 //! ByteVector = "#u8(" Byte* ')'
 //! Byte       = <any exact integer between 0 and 255>
 //! ```
-//! 
+//!
 //! The parser is defined as follows:
-//! 
+//!
 //! ```rust
 //! use kamo::parser::{code, prelude::*, Input, ParseError, ParseResult, Span};
-//! 
+//!
 //! fn main() {
 //!     assert_eq!(
 //!         parse_bytevec(Input::new("#u8(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)")),
@@ -159,7 +159,7 @@
 //!         ))
 //!     );
 //! }
-//! 
+//!
 //! fn parse_bytevec(input: Input<'_>) -> ParseResult<'_, Vec<u8>> {
 //!     context_as(
 //!         delimited(
@@ -175,7 +175,7 @@
 //!         "expecting a byte vector: #u8(<byte>*)",
 //!     )(input)
 //! }
-//! 
+//!
 //! fn parse_bytevec_value(input: Input<'_>) -> ParseResult<'_, u8> {
 //!     let result = preceded(
 //!         ascii::whitespace0,
@@ -187,7 +187,7 @@
 //!             parse_natural,
 //!         )),
 //!     )(input);
-//! 
+//!
 //!     match result {
 //!         Ok((value, cursor)) => {
 //!             if value > u8::MAX as u64 {
@@ -203,41 +203,41 @@
 //!         Err(err) => Err(err),
 //!     }
 //! }
-//! 
+//!
 //! #[inline]
 //! fn parse_binary_natural(input: Input<'_>) -> ParseResult<u64> {
 //!     preceded(tag("#b"), literal::natural(literal::Radix::Binary))(input)
 //! }
-//! 
+//!
 //! #[inline]
 //! fn parse_octal_natural(input: Input<'_>) -> ParseResult<u64> {
 //!     preceded(tag("#o"), literal::natural(literal::Radix::Octal))(input)
 //! }
-//! 
+//!
 //! #[inline]
 //! fn parse_decimal_natural(input: Input<'_>) -> ParseResult<u64> {
 //!     preceded(tag("#d"), parse_natural)(input)
 //! }
-//! 
+//!
 //! #[inline]
 //! fn parse_hexadecimal_natural(input: Input<'_>) -> ParseResult<u64> {
 //!     preceded(tag("#x"), literal::natural(literal::Radix::Hexadecimal))(input)
 //! }
-//! 
+//!
 //! #[inline]
 //! fn parse_natural(input: Input<'_>) -> ParseResult<u64> {
 //!     literal::natural(literal::Radix::Decimal)(input)
 //! }
 //! ```
-//! 
+//!
 //! # Macros
-//! 
+//!
 //! Macros are available when the `macros` feature is enabled. It is not
 //! enabled by default.
-//! 
+//!
 //! The macros are used to parse a string literal into a single or an array of
 //! `kamo::value::Value`s. The macros are defined as follows:
-//! 
+//!
 //! - `sexpr!(<mutator>, <expression>)` - A macro for parsing a single
 //!   s-expression from a string. It returns a `kamo::value::Value`.
 //!
@@ -287,7 +287,8 @@
 //! ## Examples
 //!  
 //! ```rust
-//! use kamo::{mem::Mutator, sexpr, value::{print, Value}};
+//! use kamo::{mem::Mutator, value::{print, Value}};
+//! use kamo_macros::sexpr;
 //!  
 //! let m = Mutator::new_ref();
 //! let value = sexpr!(m, "(1 2 3)");
@@ -296,21 +297,23 @@
 //! ```
 //!  
 //! ```rust
-//! use kamo::{sexpr_file, value::{print, Value}};
+//! use kamo::value::{print, Value};
+//! use kamo_macros::sexpr_file;
 //!  
-//! let values: &[Value] = &sexpr_file!("tests/sexpr/values.scm");
+//! let values: &[Value] = &sexpr_file!("tests/macros/values.scm");
 //!  
 //! assert_eq!(values.len(), 3);
 //! assert_eq!(print(values[0].clone()).to_string(), "()");
 //! assert_eq!(print(values[1].clone()).to_string(), "100");
 //! assert_eq!(print(values[2].clone()).to_string(), "#t");
-//! 
-//! let values: &[Value] = &sexpr_file!("tests/sexpr/empty.scm");
+//!
+//! let values: &[Value] = &sexpr_file!("tests/macros/empty.scm");
 //! assert_eq!(values.len(), 0);
 //! ```
 //!  
 //! ```rust
-//! use kamo::{mem::Mutator, sexpr_script, value::{print, Value}};
+//! use kamo::{mem::Mutator, value::{print, Value}};
+//! use kamo_macros::sexpr_script;
 //!  
 //! let m = Mutator::new_ref();
 //! let values: &[Value] = &sexpr_script!(m, "(define a 1)\n(define b 2)\n(+ a b)");
@@ -323,9 +326,9 @@
 //! let values: &[Value] = &sexpr_script!("");
 //! assert_eq!(values.len(), 0);
 //! ```
-//! 
+//!
 //! # Feature List
-//! 
+//!
 //! - [x] Module `kamo::parser` for parsing UTF-8 text. A parser combinator
 //! library for parsing UTF-8 text in a safe and mostly zero-copy way.
 //! - [x] Module `kamo::mem` for automatic memory management. Values are allocated
@@ -430,14 +433,14 @@ mod tests {
 
     #[test]
     fn sexpr_file_success() {
-        let values: &[Value] = &sexpr_file!("tests/sexpr/values.scm");
+        let values: &[Value] = &sexpr_file!("tests/macros/values.scm");
 
         assert_eq!(values.len(), 3);
         assert_eq!(print(values[0].clone()).to_string(), "()");
         assert_eq!(print(values[1].clone()).to_string(), "100");
         assert_eq!(print(values[2].clone()).to_string(), "#t");
 
-        let values: &[Value] = &sexpr_file!("tests/sexpr/empty.scm");
+        let values: &[Value] = &sexpr_file!("tests/macros/empty.scm");
 
         assert_eq!(values.len(), 0);
     }
